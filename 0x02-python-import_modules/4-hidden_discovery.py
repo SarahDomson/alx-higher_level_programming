7ecb2679cd1e0b10c1cafbd808214204fa3ad4e1
Sarah Domson <sarahdomson26@gmail.com>
2023-03-11 12:35:27 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-    print(a)
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
+
